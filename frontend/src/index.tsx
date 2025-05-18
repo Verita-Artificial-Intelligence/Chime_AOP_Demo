222e20062723a57614b65d321bebdf90ad78373d
Nicholas Van Landschoot <nicholas@apigen.com>
2025-05-18 07:14:02 -0600

    fixed clerk env

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ClerkProvider } from '@clerk/clerk-react';
-import { CLERK_FRONTEND_API } from './config';
+import { CLERK_PUBLISHABLE_KEY } from './config';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <ClerkProvider frontendApi={CLERK_FRONTEND_API} navigate={(to) => window.history.pushState(null, '', to)}>
+  <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} navigate={(to) => window.history.pushState(null, '', to)}>
     <Router>
       <App />
     </Router>
