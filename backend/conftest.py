02e3414b09dba3a623f37618c86d32ac3505fe1b
Nicholas Van Landschoot <135360226+nickvanlandschoot@users.noreply.github.com>
2025-05-18 07:46:34 -0600

    Remove legacy auth tests and update fixtures

--- backend/conftest.py
+++ backend/conftest.py
@@ -3,11 +3,9 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database, drop_database
 from fastapi.testclient import TestClient
-import typing as t
 
-from app.core import config, security
+from app.core import config
 from app.db.session import Base, get_db
-from app.db import models
 from app.main import app
 
 
@@ -87,83 +85,3 @@ def client(test_db):
     yield TestClient(app)
 
 
-@pytest.fixture
-def test_password() -> str:
-    return "securepassword"
-
-
-def get_password_hash() -> str:
-    """
-    Password hashing can be expensive so a mock will be much faster
-    """
-    return "supersecrethash"
-
-
-@pytest.fixture
-def test_user(test_db) -> models.User:
-    """
-    Make a test user in the database
-    """
-
-    user = models.User(
-        email="fake@email.com",
-        hashed_password=get_password_hash(),
-        is_active=True,
-    )
-    test_db.add(user)
-    test_db.commit()
-    return user
-
-
-@pytest.fixture
-def test_superuser(test_db) -> models.User:
-    """
-    Superuser for testing
-    """
-
-    user = models.User(
-        email="fakeadmin@email.com",
-        hashed_password=get_password_hash(),
-        is_superuser=True,
-    )
-    test_db.add(user)
-    test_db.commit()
-    return user
-
-
-def verify_password_mock(first: str, second: str) -> bool:
-    return True
-
-
-@pytest.fixture
-def user_token_headers(
-    client: TestClient, test_user, test_password, monkeypatch
-) -> t.Dict[str, str]:
-    monkeypatch.setattr(security, "verify_password", verify_password_mock)
-
-    login_data = {
-        "username": test_user.email,
-        "password": test_password,
-    }
-    r = client.post("/api/token", data=login_data)
-    tokens = r.json()
-    a_token = tokens["access_token"]
-    headers = {"Authorization": f"Bearer {a_token}"}
-    return headers
-
-
-@pytest.fixture
-def superuser_token_headers(
-    client: TestClient, test_superuser, test_password, monkeypatch
-) -> t.Dict[str, str]:
-    monkeypatch.setattr(security, "verify_password", verify_password_mock)
-
-    login_data = {
-        "username": test_superuser.email,
-        "password": test_password,
-    }
-    r = client.post("/api/token", data=login_data)
-    tokens = r.json()
-    a_token = tokens["access_token"]
-    headers = {"Authorization": f"Bearer {a_token}"}
-    return headers
